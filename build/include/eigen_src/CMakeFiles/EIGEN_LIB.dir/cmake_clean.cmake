6684cf1cb09edf1e5e87cf150c6a86d815bed23c
kalter <kalter0901@gmail.com>
2024-03-03 14:01:17 +0300

    Removed build package

--- build/include/eigen_src/CMakeFiles/EIGEN_LIB.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "libEIGEN_LIB.a"
-  "libEIGEN_LIB.pdb"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/EIGEN_LIB.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/include/spdlog_src/CMakeFiles/SPDLOG_LIB.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libSPDLOG_LIB.a"
-)

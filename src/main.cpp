f9b923cb1910e52d2234c81dc64397e7479963b0
kalter <kalter0901@gmail.com>
2024-03-03 19:15:50 +0300

    Changed compilation for eigen and spdlog (they should compile acording their CMakeLists.txt)

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,21 @@
 #include "nn_service/nn_service.h"
 
-int main() {
-    NNService service("http://127.0.0.1:8081");
-    service.run_nn_service("127.0.0.1", 9090);
-    while (true)
-    {
-        /* code */
+int
+main(int amount, const char** args) {
+    const char* serverHost;
+    const char* clientHost;
+    if (amount < 3) {
+        serverHost = "127.0.0.1";
+        clientHost = "http://127.0.0.1:8081";
     }
-    
+    else if (amount > 3) {std::exit(-1); }
+    else {
+        serverHost = args[1];
+        clientHost = args[2];
+    }
+
+    NNService service(clientHost);
+    service.run_nn_service(serverHost, 9090);
+
     return 0;
 }
